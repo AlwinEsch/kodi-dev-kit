c9cf0888aa6fb501dfacf8ff779ea0fd7b0bdf5c
Alwin Esch <alwin.esch@web.de>
2017-09-13 01:22:40 +0200

    remove wrong added build folder

--- build/src/javascript/CMakeFiles/npmpkg.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "kodidevkitjsJAVASCRIPT_wrap.cxx"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/npmpkg.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
